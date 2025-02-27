68db8efa7cbbbe95d863f4c5386c7a1f2c86a6ce
Ysmmm <1850955441@qq.com>
2025-02-27 22:18:57 +0800

    Initial commit

--- app/frontend/biz/service/home.go
+++ app/frontend/biz/service/home.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	common "github.com/cloudwego/biz-demo/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/cloudwego/biz-demo/gomall/app/frontend/infra/rpc"
+	"github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/product"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 )
 
 type HomeService struct {
@@ -16,18 +19,14 @@ func NewHomeService(Context context.Context, RequestContext *app.RequestContext)
 	return &HomeService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *HomeService) Run(req *common.Empty) (map[string]any, error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
-	var resp = make(map[string]any)
-	items := []map[string]any{
-		{"Name": "pen-1", "Price": 100, "Picture": "/static/image/pen-1.png"},
-		{"Name": "pen-2", "Price": 110, "Picture": "/static/image/pen-2.png"},
-		{"Name": "pen-3", "Price": 120, "Picture": "/static/image/pen-3.png"},
+func (h *HomeService) Run(req *common.Empty) (res map[string]any, err error) {
+	products, err := rpc.ProductClient.ListProducts(h.Context, &product.ListProductsReq{})
+	if err != nil {
+		return nil, err
 	}
-	resp["Title"] = "Hot Sales"
-	resp["Items"] = items
-	return resp, nil
+
+	return utils.H{
+		"title": "Hot sale",
+		"items": products.Products,
+	}, nil
 }
--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -4,7 +4,8 @@ import (
 	"sync"
 
 	"github.com/cloudwego/biz-demo/gomall/app/frontend/conf"
-	frontendUtils "github.com/cloudwego/biz-demo/gomall/app/frontend/utils"
+	frontendutils "github.com/cloudwego/biz-demo/gomall/app/frontend/utils"
+	"github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/product/productcatalogservice"
 	"github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/user/userservice"
 	"github.com/cloudwego/kitex/client"
 	consul "github.com/kitex-contrib/registry-consul"
@@ -12,20 +13,31 @@ import (
 
 var (
 	UserClient userservice.Client
-
+	ProductClient productcatalogservice.Client
 	once sync.Once
 )
 
 func Init() {
 	once.Do(func() {
 		initUserClient()
+		initProductClient()
 	})
 }
 
 func initUserClient() {
 	
 	r, err := consul.NewConsulResolver(conf.GetConf().Hertz.RegistryAddr)
-	frontendUtils.MustHandleError(err)
+	frontendutils.MustHandleError(err)
 	UserClient, err = userservice.NewClient("user", client.WithResolver(r))
-	frontendUtils.MustHandleError(err)
+	frontendutils.MustHandleError(err)
+}
+
+func initProductClient() {
+	var opts []client.Option
+	
+	r, err := consul.NewConsulResolver(conf.GetConf().Hertz.RegistryAddr)
+	frontendutils.MustHandleError(err)
+	opts = append(opts, client.WithResolver(r))
+	ProductClient, err = productcatalogservice.NewClient("product", opts...)
+	frontendutils.MustHandleError(err)
 }
